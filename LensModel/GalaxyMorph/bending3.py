be22bb1e206b5bf8c4360c4308933b19e17e805f
pastroe <dcz_9@mail.ustc.edu.cn>
2022-01-03 01:19:28 +0800

    Add edge-on disk profile and a class for multiple image.

---
+++ LensModel/GalaxyMorph/bending3.py
@@ -0,0 +1,40 @@
+__author__ = 'not sibirrer'
+
+
+import numpy as np
+from lenstronomy.LensModel.Profiles.base_profile import LensProfileBase
+
+__all__ = ['Bending3']
+
+
+class Bending3(LensProfileBase):
+    """
+    class to compute the Bending Correction with power-law (alpha = 3).
+    """
+    param_names = ['Strength', 'center_x', 'center_y']
+    lower_limit_default = {'Strength': -100, 'center_x': -100, 'center_y': -100}
+    upper_limit_default = {'Strength':  100, 'center_x': 100, 'center_y': 100}
+
+    def __init__(self):
+        self.r_min = 10**(-25)
+        super(Bending3, self).__init__()
+
+    def mapping(self, x, y, Strength, center_x=0, center_y=0):
+        """
+
+        :param x: x-coord (in angles)
+        :param y: y-coord (in angles)
+        :param Strength: Einstein radius (in angles)
+        :return: deflection angle (in angles)
+        """
+        x_ = x - center_x
+        y_ = y - center_y
+        return x_ + center_x, y_ + Strength * x_ ** 3 + center_y
+        
+    def param_dict(self, index):
+        p_dict = {}
+        for param in self.param_names:
+            if param not in ['center_x', 'center_y']:
+                p_dict[param] = param  + '_' + str(index)
+        return p_dict
+        
